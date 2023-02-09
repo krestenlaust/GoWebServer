d273450aae2d40e4ed2c3aac93aef1204f60b197
Kresten Laust <krestenlaust@gmail.com>
2023-02-09 17:18:32 +0100

    Listener- and parsing code written

--- main.go
+++ main.go
@@ -4,11 +4,40 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	fmt.Println("Hello, world")
+}
+
+func listenHttp(host string, port int) error {
+	hostport := net.JoinHostPort(host, strconv.Itoa(port))
+	l, err := net.Listen("tcp", hostport)
+
+	if err != nil {
+		return err
+	}
+
+	defer l.Close()
+	fmt.Println("Listening on " + hostport)
+
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error: " + err.Error())
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+
 }
 
 // TODO: should receive the network connection instead of a parsed request,
@@ -44,6 +73,40 @@ type Request struct {
 	userAgent  string
 	host       string
 	originator net.IP
+}
+
+func ParseRequestHeader(rawHeader string) (Request, error) {
+	res := new(Request)
+	lines := strings.Split(rawHeader, "\r\n")
+
+	for i := 0; i < len(lines); i++ {
+		if strings.HasSuffix(strings.ToLower(lines[i]), "http/1.1") {
+			words := strings.Split(lines[i], " ")
+
+			res.method = strings.ToLower(words[0])
+			res.requestUri = words[1]
+			continue
+		}
+
+		keyValuePair := strings.SplitN(lines[i], ":", 2)
+
+		switch strings.ToLower(keyValuePair[0]) {
+		case "user-agent":
+			res.userAgent = keyValuePair[1]
+		case "host":
+			res.host = keyValuePair[1]
+		case "content-length":
+			i, err := strconv.Atoi(strings.TrimSpace(keyValuePair[i]))
+
+			if err != nil {
+				return *res, errors.New("Invalid request header, 'Content-Length' value not integral")
+			}
+
+			res.contentLength = i
+		}
+	}
+
+	return *res, nil
 }
 
 type Response struct {
