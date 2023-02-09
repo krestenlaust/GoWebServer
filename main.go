a31a88f9779dab3aef9706896e246abe950c5fe2
Kresten Laust <krestenlaust@gmail.com>
2023-02-09 12:25:52 +0100

    I had misunderstood the syntax of structs

--- main.go
+++ main.go
@@ -2,48 +2,48 @@ package main
 
 import (
 	"fmt"
-    "time"
-    "net"
+	"net"
+	"time"
 )
 
 func main() {
-    fmt.Println("Hello, world")
+	fmt.Println("Hello, world")
 }
 
 type Request struct {
-    // General header
-    int contentLength
-    []byte body
-    string connectionStatus // 'close', 'keep-alive' etc.
-    Time date
+	// General header
+	contentLength    int
+	body             []byte
+	connectionStatus string // 'close', 'keep-alive' etc.
+	date             time.Time
 
-    // Request header
-    string method
-    string requestUri
-    string userAgent
-    string host
-    IP originator
+	// Request header
+	method     string
+	requestUri string
+	userAgent  string
+	host       string
+	originator net.IP
 }
 
 type Response struct {
-    // General header
-    int contentLength
-    []byte body
-    string connectionStatus
-    Time date
+	// General header
+	contentLength    int
+	body             []byte
+	connectionStatus string // 'close', 'keep-alive' etc.
+	date             time.Time
 
-    // Response header
-    int statusCode
-    string statusResponse
-    string contentType
-    string content
+	// Response header
+	statusCode     int
+	statusResponse string
+	contentType    string
+	content        string
 }
 
 func (res Response) String() string {
-    return fmt.Sprintf("HTTP/1.1 %d %s\r\n", res.statusCode, res.statusResponse) +
-    	fmt.Sprintf("Date: \r\n", res.date.Format("ddd, dd MMM yyyy T")) +
-	fmt.Sprintf("Connection: %s\r\n", res.connectionStatus) +
-	fmt.Sprintf("Content-Length: %s\r\n", res.contentLength) +
-	fmt.Sprintf("Content-Type: %s\r\n\r\n", res.contentType) +
-	res.content
+	return fmt.Sprintf("HTTP/1.1 %d %s\r\n", res.statusCode, res.statusResponse) +
+		fmt.Sprintf("Date: %s\r\n", res.date.Format("Mon, 01 Jan 2006 15:04:05")) +
+		fmt.Sprintf("Connection: %s\r\n", res.connectionStatus) +
+		fmt.Sprintf("Content-Length: %d\r\n", res.contentLength) +
+		fmt.Sprintf("Content-Type: %s\r\n\r\n", res.contentType) +
+		res.content
 }
